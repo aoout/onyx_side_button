2158038b9080f43a78c742dea60fbee23e9c13a9
aoout <wuz66280@gmail.com>
2024-10-17 23:04:48 +0800

    feat: add fliping bias settings

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { MarkdownView, Plugin } from "obsidian";
+import { OnyxSideButtonSettings, DEFAULT_SETTINGS } from "./settings/settings";
+import { OnyxSideButtonSettingsTab } from "./settings/settingsTab";
 
 export default class OnyxSideButton extends Plugin {
+	settings: OnyxSideButtonSettings;
 	lastPageY: number;
 	async onload() {
+		await this.loadSettings();
+		this.addSettingTab(new OnyxSideButtonSettingsTab(this.app, this));
 		this.registerEvent(
 			this.app.workspace.on("file-open", (_) => {
 				const viewEl = document.querySelector(".markdown-reading-view") as HTMLElement;
@@ -24,11 +29,19 @@ export default class OnyxSideButton extends Plugin {
 		);
 	}
 
+	async loadSettings(): Promise<void> {
+		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+	}
+
+	async saveSettings(): Promise<void> {
+		await this.saveData(this.settings);
+	}
+
 	flip(mode: string) {
 		const thisView = this.app.workspace.getActiveViewOfType(MarkdownView);
 		// @ts-ignore
 		const thisScrollObj = thisView?.previewMode?.renderer?.previewEl;
-		const range = thisScrollObj?.clientHeight - 60;
+		const range = thisScrollObj?.clientHeight - this.settings.flipingBias;
 		thisScrollObj?.scrollBy(0, mode === "up" ? -range : range);
 	}
 }
